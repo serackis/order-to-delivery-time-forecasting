0d85c357a07bd88e49d4ffe411cf3fe8de622169
viraj.modak <viraj.modak@accenture.com>
2022-11-03 16:20:16 -0700

    add files for release

---
+++ src/run_benchmarks_regression.py
@@ -0,0 +1,275 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+'''
+Code for executing regression pipeline
+'''
+
+# pylint: disable=consider-using-with
+
+import argparse
+import logging
+import pathlib
+import time
+import pickle  # nosec
+import pandas as pd
+import xgboost as xgb
+from utils.data_functions import read_data, preprocessing, haversine_distance, get_package_size
+from utils.data_functions import object_to_int
+from utils.prediction import xgb_predict_reg, rf_predict_reg, SV_predict_reg, ensemble_predict_reg
+
+def main(FLAGS):
+    '''
+    ###############
+    # Data Read and Preprocessing start
+    ###############
+    '''
+
+    if FLAGS.logfile == "":
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        path = pathlib.Path(FLAGS.logfile)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        logging.basicConfig(filename=FLAGS.logfile, level=logging.DEBUG)
+
+    logger = logging.getLogger()
+
+    i_flag = FLAGS.intel
+    if i_flag:
+        from sklearnex import patch_sklearn  # pylint: disable=C0415, E0401
+        patch_sklearn()
+    
+    from sklearn.model_selection import train_test_split  # pylint: disable=C0415
+    from sklearn.model_selection import GridSearchCV  # pylint: disable=C0415
+    from sklearn.ensemble import RandomForestRegressor  # pylint: disable=C0415
+    from sklearn.ensemble import VotingRegressor  # pylint: disable=C0415
+    from sklearn.svm import SVR  # pylint: disable=C0415
+    
+    logger.info("\n")
+    if FLAGS.intel:
+        logger.info("===== Running benchmarks for oneAPI tech =====")
+    else:
+        logger.info("===== Running benchmarks for stock =====")
+
+    orders, items, customers, sellers, geo, products = read_data()
+    logger.info("data read done. moving to preprocessing")
+    filtered_orders = preprocessing(orders, items, customers, sellers, geo, i_flag)
+    filtered_orders['distance'] = filtered_orders.apply(lambda row: haversine_distance(row["geolocation_lng_seller"],
+                                                                                       row["geolocation_lat_seller"],
+                                                                                       row["geolocation_lng_customer"],
+                                                                                       row["geolocation_lat_customer"],),
+                                                        axis=1,)
+
+    orders_size_weight = get_package_size(items, products)
+    filtered_orders = filtered_orders.merge(orders_size_weight, on='order_id', how='left')
+
+    # process time columns
+    time_columns = ['order_purchase_timestamp', 'order_delivered_customer_date', 'order_estimated_delivery_date']
+    for column in time_columns:
+        filtered_orders.loc[:, column] = pd.to_datetime(filtered_orders[column])
+
+    filtered_orders.loc[:, "wait_time"] = (filtered_orders['order_delivered_customer_date'] -
+                                           filtered_orders['order_purchase_timestamp']).dt.days
+    filtered_orders.loc[:, "est_wait_time"] = (filtered_orders['order_estimated_delivery_date'] -
+                                               filtered_orders['order_purchase_timestamp']).dt.days
+
+    filtered_orders.loc[:, "purchase_dow"] = filtered_orders.order_purchase_timestamp.dt.dayofweek
+    filtered_orders.loc[:, "year"] = filtered_orders.order_purchase_timestamp.dt.year
+    filtered_orders.loc[:, "purchase_month"] = filtered_orders.order_purchase_timestamp.dt.month
+
+    final_df = filtered_orders[['purchase_dow', 'purchase_month', 'year', 'product_size_cm3', 'product_weight_g',
+                                'geolocation_state_customer', 'geolocation_state_seller', 'distance',
+                                'wait_time', 'est_wait_time']]
+    final_df['delay'] = final_df['wait_time'] - final_df['est_wait_time']
+    final_df['delay'] = final_df['delay'] > 0
+    final_df['delay'] = final_df['delay'].astype(int)
+
+    final_df_enc = final_df.apply(lambda x: object_to_int(x))  # pylint: disable=W0108
+    logger.info("preprocessing done")
+
+    #
+    # Data Read and Preprocessing end
+    #
+
+    # split data
+    columns_for_regression_train = ['purchase_dow', 'purchase_month', 'year', 'product_size_cm3', 'product_weight_g',
+                                    'geolocation_state_customer', 'geolocation_state_seller',
+                                    'distance']
+    columns_for_regression_pred = ['wait_time']
+
+    X_train_reg_0, X_test_reg_0, y_train_reg_0, y_test_reg_0 = train_test_split(final_df_enc[columns_for_regression_train],
+                                                                                final_df_enc[columns_for_regression_pred],
+                                                                                random_state=42)
+
+    #
+
+    # Hyperparameter Tuning for individual models (XGB, RF, SV)
+
+    #
+
+    hyper_train = pd.DataFrame(X_train_reg_0, columns=columns_for_regression_train)
+    hyper_train['wait_time'] = y_train_reg_0['wait_time'].values
+    hyper_train_section = hyper_train.sample(8192, random_state=42)
+    X_train_reg = hyper_train_section[columns_for_regression_train]
+    y_train_reg = hyper_train_section[columns_for_regression_pred]
+    
+    # Set parameters for xgb and random forests and SV for hyper parameter tuning
+    xgb_params = {'n_estimators': [500, 1000], 'max_depth': [10, 20], 'tree_method': ['hist']}
+    rf_params = {'n_estimators': [500, 1000], 'max_depth': [10, 20]}
+    SV_params = {'C': [10, 20], 'epsilon': [0.1, 0.05], 'kernel': ['rbf'], 'gamma': ['auto']}
+    ensemble_params = {'weights': [(1, 1, 1), (1, 2, 1), (2, 1, 1), (1, 1, 2)]}
+
+    logger.info('Running Hyperparameter Tuning for XGB')
+    xgb_model = xgb.XGBRegressor()
+    xgb_grid = GridSearchCV(xgb_model, xgb_params, cv=4, n_jobs=-1, verbose=True)
+    
+    # record time
+    xgb_start = time.time()
+    xgb_grid.fit(X_train_reg, y_train_reg)
+    xgb_hyper_time = time.time()-xgb_start
+    
+    # extract best grid
+    xgb_best_grid = xgb_grid.best_estimator_
+
+    logger.info('Hyperparameter tuning time for for: XGB: %s', str(xgb_hyper_time))
+    logger.info('\n')
+
+    logger.info('Running Hyperparameter Tuning for RF')
+    rf_model = RandomForestRegressor()
+    rf_grid = GridSearchCV(rf_model, rf_params, cv=4, n_jobs=-1, verbose=True)
+    
+    # record time
+    rf_start = time.time()
+    rf_grid.fit(X_train_reg, y_train_reg.values.ravel())
+    rf_hyper_time = time.time()-rf_start
+    
+    # extract best grid
+    rf_best_grid = rf_grid.best_estimator_
+
+    logger.info('Hyperparameter tuning time for for: RF: %s', str(rf_hyper_time))
+    logger.info('\n')
+    
+    logger.info('Running Hyperparameter Tuning for SV')
+    SV_model = SVR()
+    SV_grid = GridSearchCV(SV_model, SV_params, cv=4, n_jobs=-1, verbose=True)
+
+    # record time
+    SV_start = time.time()
+    SV_grid.fit(X_train_reg, y_train_reg.values.ravel())
+    SV_hyper_time = time.time()-SV_start
+    
+    # extract best grid and params
+    SV_best_grid = SV_grid.best_estimator_
+
+    logger.info('Hyperparameter tuning time for for: SV: %s', str(SV_hyper_time))
+    logger.info('\n')
+
+    #
+
+    # Hyperparameter Tuning for ensemble model using the "Best Grid for SGB, RF, SV"
+
+    #
+
+    logger.info('Running Hyperparameter Tuning for ensemble')
+    voting_model = VotingRegressor(estimators=[('xgb', xgb_best_grid), ('rf', rf_best_grid), ('SV', SV_best_grid)])
+    ensemble_grid = GridSearchCV(voting_model, ensemble_params, cv=4, n_jobs=-1, verbose=True)
+
+    # record time
+    ensemble_start = time.time()
+    ensemble_grid.fit(X_train_reg, y_train_reg.values.ravel())
+    ensemble_hyper_time = time.time()-ensemble_start
+    
+    # extract best params
+    ensemble_best_params = ensemble_grid.best_params_
+
+    logger.info('Hyperparameter tuning time for for: ensemble model: %s', str(ensemble_hyper_time))
+    logger.info('\n')
+
+    #
+
+    # Extract Simple Training Benchmarks using best parameters for ensemble
+
+    #
+
+    ti_train = pd.DataFrame(X_train_reg_0, columns=columns_for_regression_train)
+    ti_train['wait_time'] = y_train_reg_0['wait_time'].values
+    
+    for data_size in [8192, 32768]:
+        ti_train_section = ti_train.sample(data_size, random_state=42)
+        X_train_reg = ti_train_section[columns_for_regression_train]
+        y_train_reg = ti_train_section[columns_for_regression_pred]
+
+        logger.info('Running Training for ensemble model with data length %s', str(data_size))
+        
+        voting_model = VotingRegressor(estimators=[('xgb', xgb_best_grid), ('rf', rf_best_grid), ('SV', SV_best_grid)],
+                                       n_jobs=-1, weights=ensemble_best_params['weights'])
+        ensemble_model_train_start = time.time()
+        voting_model.fit(X_train_reg, y_train_reg.values.ravel())
+        ensemble_model_train_time = time.time() - ensemble_model_train_start
+        
+        logger.info('Training time for for: ensemble model: %s', str(ensemble_model_train_time))
+        logger.info('\n')
+
+    ti_test_base = pd.DataFrame(X_test_reg_0, columns=columns_for_regression_train)
+    ti_test_base['wait_time'] = y_test_reg_0['wait_time'].values
+    ti_test = pd.concat([ti_test_base, ti_test_base, ti_test_base, ti_test_base])
+
+    for data_size in [10000, 30000, 50000, 70000]:
+        ti_test_section = ti_test.sample(data_size, random_state=42)
+        X_test_reg = ti_test_section[columns_for_regression_train]
+        y_test_reg = ti_test_section[columns_for_regression_pred]
+
+        # Perform prediction - refer to ensemble train function in utils
+        xgb_mse, xgb_pred_time = xgb_predict_reg(xgb_best_grid, X_test_reg, y_test_reg)
+        rf_mse, rf_pred_time = rf_predict_reg(rf_best_grid, X_test_reg, y_test_reg)
+        SV_mse, SV_pred_time = SV_predict_reg(SV_best_grid, X_test_reg, y_test_reg)
+        ensemble_mse, ensemble_pred_time = ensemble_predict_reg(voting_model, X_test_reg, y_test_reg)
+
+        logger.info('Running Inference with data length %s', str(data_size))
+        logger.info("Inference time and MSE for for XGB: %s", ' '.join(map(str, list((xgb_pred_time, xgb_mse)))))
+        logger.info("Inference time and MSE for for RF: %s", ' '.join(map(str, list((rf_pred_time, rf_mse)))))
+        logger.info("Inference time and MSE for for SV: %s", ' '.join(map(str, list((SV_pred_time, SV_mse)))))
+        logger.info("Inference time and MSE for for Voting model: %s", ' '.join(map(str, list((ensemble_pred_time, ensemble_mse)))))
+        logger.info('\n')
+    
+    pickle.dump(voting_model, open(FLAGS.modelfile, 'wb'))  # nosec
+    voting_model = pickle.load(open(FLAGS.modelfile, 'rb'))  # nosec
+    ti_test_section = ti_test.sample(1000, random_state=42)
+    lst_of_stream_times = []
+    for _counter in range(1000):
+        sample_df = ti_test_section.sample(n=1)
+        X_test_reg = sample_df[columns_for_regression_train]
+        y_test_reg = sample_df[columns_for_regression_pred]
+        ensemble_mse, ensemble_pred_time = ensemble_predict_reg(voting_model, X_test_reg, y_test_reg)  # pylint: disable=W0612
+        lst_of_stream_times.append(ensemble_pred_time)
+    avg_stream_time = sum(lst_of_stream_times)/len(lst_of_stream_times)
+    logger.info("Average Streaming Inference Time for Voting Regressor model: %s", str(avg_stream_time))
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l',
+                        '--logfile',
+                        type=str,
+                        default="",
+                        help="log file to output benchmarking results to")
+
+    parser.add_argument('-m',
+                        '--modelfile',
+                        type=str,
+                        default="",
+                        required=False,
+                        help="dump final voting model")
+
+    parser.add_argument('-i',
+                        '--intel',
+                        default=False,
+                        action="store_true",
+                        help="use intel accelerated technologies")
+    
+    FLAGS = parser.parse_args()
+
+    pd.options.mode.chained_assignment = None  # default='warn'
+    
+    main(FLAGS)
---
+++ src/utils/data_functions.py
@@ -0,0 +1,103 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+'''
+Utility functions for data processing
+'''
+
+from math import radians, sin, cos, asin, sqrt
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+def read_data():
+    orders = pd.read_csv("./data/olist_orders_dataset.csv")
+    items = pd.read_csv("./data/olist_order_items_dataset.csv")
+    customers = pd.read_csv("./data/olist_customers_dataset.csv")
+    sellers = pd.read_csv("./data/olist_sellers_dataset.csv")
+    geo = pd.read_csv("./data/olist_geolocation_dataset.csv")
+    products = pd.read_csv("./data/olist_products_dataset.csv")
+    return orders, items, customers, sellers, geo, products
+
+
+def preprocessing(orders, items, customers, sellers, geo, i_flag):
+    # Merge/Clean Data
+
+    # Get the seller zip code of each order
+    middle = items[['order_id', 'seller_id']]
+    middle_2 = middle.merge(sellers[['seller_id', 'seller_zip_code_prefix']], on="seller_id", how="outer")
+    orders = orders.merge(middle_2, on="order_id", how="left")
+
+    # Get customer zip code of each order
+    orders = orders.merge(customers[['customer_id', 'customer_zip_code_prefix']],
+                          on='customer_id', how="left")
+
+    # Clean geo df
+    geo = geo[~geo['geolocation_zip_code_prefix'].duplicated()]
+
+    # add seller coordinates to the orders
+    orders = orders.merge(geo, left_on="seller_zip_code_prefix",
+                          right_on="geolocation_zip_code_prefix", how="left")
+
+    # add customer coordinates to the orders
+    orders = orders.merge(geo, left_on="customer_zip_code_prefix",
+                          right_on="geolocation_zip_code_prefix", how="left",
+                          suffixes=("_seller", "_customer"))
+    # Clean orders
+    # 1-Filter out orders with multiple sellers Because each order only has one delivery date
+    df = orders.groupby(by="order_id").nunique()
+    mono_orders = pd.Series(df[df['seller_id'] == 1].index)
+    filtered_orders = orders.merge(mono_orders, how='inner')
+
+    # 2-drop rows with missing values
+    filtered_orders = filtered_orders.drop(columns=["order_approved_at"])
+    filtered_orders = filtered_orders.dropna()
+    return filtered_orders
+
+
+# Define Function to calculate distance
+def haversine_distance(lon1, lat1, lon2, lat2):
+    """
+    Compute distance between two pairs of (lat, lng)
+    """
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    return 2 * 6371 * asin(sqrt(a))
+
+
+def get_package_size(items, products):
+    # Get Package Size
+    df_tmp = items[['order_id', 'product_id']].merge(products[['product_id', 'product_length_cm', 'product_height_cm',
+                                                               'product_width_cm', 'product_weight_g']],
+                                                     on="product_id",
+                                                     how="outer")
+    df_tmp.loc[:, "product_size_cm3"] = \
+        df_tmp['product_length_cm']*df_tmp['product_width_cm'] * df_tmp['product_height_cm']
+    orders_size_weight = df_tmp.groupby("order_id", as_index=False).sum()[['order_id', 'product_size_cm3',
+                                                                           'product_weight_g']]
+    return orders_size_weight
+
+def object_to_int(dataframe_series):
+
+    if dataframe_series.dtype == 'object':
+        dataframe_series = LabelEncoder().fit_transform(dataframe_series)
+    return dataframe_series
+
+
+def split_data(final_df, columns_for_train, columns_for_pred, i_flag):
+
+    if i_flag:
+        from sklearnex import patch_sklearn  # pylint: disable=C0415, E0401
+        patch_sklearn()
+    from sklearn.model_selection import train_test_split  # pylint: disable=C0415
+
+    x_train, x_test, y_train, y_test = train_test_split(final_df[columns_for_train], final_df[columns_for_pred],
+                                                        test_size=0.3, random_state=42)
+    return x_train, x_test, y_train, y_test
+    
+def scaling(X_train, X_test):
+    sc_X = StandardScaler()
+    X_train_scaled = sc_X.fit_transform(X_train)
+    X_test_scaled = sc_X.fit_transform(X_test)
+    return X_train_scaled, X_test_scaled
